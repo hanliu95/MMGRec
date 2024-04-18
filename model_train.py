bedcdb9b6f71102bd755da6ed19f2f3fd53c8450
hanliu95 <38929265+hanliu95@users.noreply.github.com>
2024-04-18 11:42:47 +0800

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -157,16 +157,13 @@ class MultiHeadAttention(nn.Module):
         # (B, S, D) -proj-> (B, S, D_new) -split-> (B, S, H, W) -trans-> (B, H, S, W)
         Q = self.W_Q(input_Q).view(batch_size, -1, n_heads, d_k).transpose(1, 2)  # Q: [batch_size, n_heads, len_q, d_k]
         K = self.W_K(input_K).view(batch_size, -1, n_heads, d_k).transpose(1, 2)  # K: [batch_size, n_heads, len_k, d_k]
-        V = self.W_V(input_V).view(batch_size, -1, n_heads, d_v).transpose(1,
-                                                                           2)  # V: [batch_size, n_heads, len_v(=len_k), d_v]
+        V = self.W_V(input_V).view(batch_size, -1, n_heads, d_v).transpose(1, 2)  # V: [batch_size, n_heads, len_v(=len_k), d_v]
 
-        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1,
-                                                  1)  # attn_mask : [batch_size, n_heads, seq_len, seq_len]
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1, 1)  # attn_mask : [batch_size, n_heads, seq_len, seq_len]
 
         # context: [batch_size, n_heads, len_q, d_v], attn: [batch_size, n_heads, len_q, len_k]
         context, attn = ScaledDotProductAttention()(Q, K, V, attn_mask)
-        context = context.transpose(1, 2).reshape(batch_size, -1,
-                                                  n_heads * d_v)  # context: [batch_size, len_q, n_heads * d_v]
+        context = context.transpose(1, 2).reshape(batch_size, -1, n_heads * d_v)  # context: [batch_size, len_q, n_heads * d_v]
         output = self.fc(context)  # [batch_size, len_q, d_model]
         output = self.dropout(output)
         return nn.LayerNorm(d_model).cuda()(output + residual), attn
@@ -201,9 +198,21 @@ class EncoderLayer(nn.Module):
         self.U_Q = nn.Linear(d_model, d_k, bias=False)
         self.U_K = nn.Linear(d_model, d_k, bias=False)
         self.U_V = nn.Linear(d_model, d_v, bias=False)
-        self.sca_q = nn.Linear(d_model, 1, bias=False)
-        self.sca_k = nn.Linear(d_model, 1, bias=False)
-        self.sca_v = nn.Linear(d_model, 1, bias=False)
+        self.sca_q = nn.Sequential(
+            nn.Linear(d_model, d_model/2, bias=False),
+            nn.ReLU(),
+            nn.Linear(d_model/2, 1, bias=False)
+        )
+        self.sca_k = nn.Sequential(
+            nn.Linear(d_model, d_model/2, bias=False),
+            nn.ReLU(),
+            nn.Linear(d_model/2, 1, bias=False)
+        )
+        self.sca_v = nn.Sequential(
+            nn.Linear(d_model, d_model/2, bias=False),
+            nn.ReLU(),
+            nn.Linear(d_model/2, 1, bias=False)
+        )
         self.fc = nn.Linear(n_heads * d_v, d_model, bias=False)
         self.pos_ffn = PoswiseFeedForwardNet()
         self.dropout = nn.Dropout(p=0.2)
